5bd4e1363f9cd7a559d1d506c4fcd0845f1d79d7
Mahmoud Ahmed Abdelghani <168209503+mahmoudAbdelghani2@users.noreply.github.com>
2024-12-06 15:08:18 +0200

    Update Gallary
    Big update in the gallery screen to make its appearance more beautiful.

--- Screens/lib/screens/Gallary.dart
+++ Screens/lib/screens/Gallary.dart
@@ -2,206 +2,163 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-void main(){
+
+
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          elevation: 20,
           title: const Text(
-            "Gallery"
-            ,style: TextStyle(
-              fontSize:30,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.bold
+            "Gallery",
+            style: TextStyle(
+                fontSize: 30,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
           ),
-            textAlign: TextAlign.center,),
           backgroundColor: Colors.white,
           foregroundColor: Colors.black,
+          centerTitle: true,
+          leading: Icon(Icons.menu, color: Colors.black),
+          actions: const [
+            Icon(Icons.settings, color: Colors.black),
+          ],
         ),
         body: Container(
-          color: Colors.white,
+          decoration: BoxDecoration(
+            color: Colors.white38,
+            borderRadius: BorderRadius.circular(50),
+          ),
           padding: EdgeInsets.all(15),
-          child: GridView.count(
-            crossAxisCount: 2,
+          child: Column(
             children: [
-
               Container(
+                height: 75,
+                width: 400,
                 margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img1.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img2.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img3.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img1.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img2.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img3.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img1.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img2.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img3.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img1.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img2.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img3.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img1.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img2.jpg",
-                  fit: BoxFit.cover,
-                ),
-              ),Container(
-                margin: EdgeInsets.all(10),
-                width: 120,
-                height: 120,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Image.asset(
-                  "images/img3.jpg",
-                  fit: BoxFit.cover,
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: const Text(
+                      "Foot Ball",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {},
+                  ),
                 ),
               ),
-
+              Container(
+                height: 75,
+                width: 400,
+                margin: EdgeInsets.all(10),
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: const Text(
+                      "Aboutrika ",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Container(
+                height: 75,
+                width: 400,
+                margin: EdgeInsets.all(10),
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: const Text(
+                      "Nature",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Container(
+                height: 75,
+                width: 400,
+                margin: EdgeInsets.all(10),
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: const Text(
+                      "Mountains",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
+              Container(
+                height: 75,
+                width: 400,
+                margin: EdgeInsets.all(10),
+                child: Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  margin: EdgeInsets.all(10),
+                  child: ListTile(
+                    title: const Text(
+                      "Empty",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 25,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    onTap: () {},
+                  ),
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-
 }

abc0ee60540469a8c482363fb88018926356552a
Mahmoud Ahmed Abdelghani <168209503+mahmoudAbdelghani2@users.noreply.github.com>
2024-12-07 11:45:43 +0200

    Add photos to gallary and update in gallary

--- Screens/lib/screens/Gallary.dart
+++ Screens/lib/screens/Gallary.dart
@@ -3,162 +3,221 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          elevation: 20,
-          title: const Text(
-            "Gallery",
-            style: TextStyle(
-                fontSize: 30,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          centerTitle: true,
-          leading: Icon(Icons.menu, color: Colors.black),
-          actions: const [
-            Icon(Icons.settings, color: Colors.black),
-          ],
+      home: GalleryPage(),
+    );
+  }
+}
+
+class GalleryPage extends StatelessWidget {
+  final List<String> buttons = ["GOAT", "Aboutrika", "Nature", "Mountains", "Foot Ball"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple[50],
+        leading: Icon(Icons.menu, color: Colors.black),
+        title: const Text(
+          "Gallery",
+          style: TextStyle(color: Colors.black),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.white38,
-            borderRadius: BorderRadius.circular(50),
-          ),
-          padding: EdgeInsets.all(15),
-          child: Column(
-            children: [
-              Container(
-                height: 75,
-                width: 400,
-                margin: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: const Text(
-                      "Foot Ball",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings, color: Colors.black),
+          )
+        ],
+      ),
+      body: Center(
+        child: ListView.builder(
+          padding: EdgeInsets.symmetric(vertical: 16),
+          itemCount: buttons.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ImagePage(title: buttons[index]),
                     ),
-                    onTap: () {},
+                  );
+                },
+                color: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                elevation: 2,
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  buttons[index],
+                  style: const TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 16,
                   ),
                 ),
               ),
-              Container(
-                height: 75,
-                width: 400,
-                margin: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: const Text(
-                      "Aboutrika ",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onTap: () {},
-                  ),
-                ),
-              ),
-              Container(
-                height: 75,
-                width: 400,
-                margin: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: const Text(
-                      "Nature",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onTap: () {},
-                  ),
-                ),
-              ),
-              Container(
-                height: 75,
-                width: 400,
-                margin: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: const Text(
-                      "Mountains",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onTap: () {},
-                  ),
-                ),
-              ),
-              Container(
-                height: 75,
-                width: 400,
-                margin: EdgeInsets.all(10),
-                child: Card(
-                  elevation: 5,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30)),
-                  margin: EdgeInsets.all(10),
-                  child: ListTile(
-                    title: const Text(
-                      "Empty",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 25,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onTap: () {},
-                  ),
-                ),
-              ),
-            ],
-          ),
+            );
+          },
         ),
+      ),
+      backgroundColor: Colors.purple[50],
+    );
+  }
+}
+
+class ImagePage extends StatelessWidget {
+  final String title;
+
+  ImagePage({required this.title});
+
+  final Map<String, List<String>> images = {
+    "Foot Ball": [
+      "assets/images/salah.png",
+      "assets/images/fede.png",
+      "assets/images/biligol.png",
+      "assets/images/salah.png",
+      "assets/images/fede.png",
+      "assets/images/biligol.png",
+      "assets/images/salah.png",
+      "assets/images/fede.png",
+      "assets/images/biligol.png",
+      "assets/images/salah.png",
+      "assets/images/fede.png",
+      "assets/images/biligol.png",
+
+    ],
+    "Aboutrika": [
+      "assets/images/trika1.png",
+      "assets/images/trika2.png",
+      "assets/images/trika3.png",
+      "assets/images/trika1.png",
+      "assets/images/trika2.png",
+      "assets/images/trika3.png",
+      "assets/images/trika1.png",
+      "assets/images/trika2.png",
+      "assets/images/trika3.png",
+      "assets/images/trika1.png",
+      "assets/images/trika2.png",
+      "assets/images/trika3.png",
+
+    ],
+    "Nature": [
+      "assets/images/img1.png",
+      "assets/images/img2.png",
+      "assets/images/img3.png",
+      "assets/images/img1.png",
+      "assets/images/img2.png",
+      "assets/images/img3.png",
+      "assets/images/img1.png",
+      "assets/images/img2.png",
+      "assets/images/img3.png",
+      "assets/images/img1.png",
+      "assets/images/img2.png",
+      "assets/images/img3.png",
+    ],
+    "Mountains": [
+      "assets/images/ountain1.png",
+      "assets/images/mountain2.png",
+      "assets/images/mountain3.png",
+      "assets/images/ountain1.png",
+      "assets/images/mountain2.png",
+      "assets/images/mountain3.png",
+      "assets/images/ountain1.png",
+      "assets/images/mountain2.png",
+      "assets/images/mountain3.png",
+      "assets/images/ountain1.png",
+      "assets/images/mountain2.png",
+      "assets/images/mountain3.png",
+    ],
+    "GOAT": [
+      "assets/images/cristiano1.png",
+      "assets/images/cristiano2.png",
+      "assets/images/cristiano3.png",
+      "assets/images/cristiano4.png",
+      "assets/images/cristiano5.png",
+      "assets/images/cristiano1.png",
+      "assets/images/cristiano2.png",
+      "assets/images/cristiano3.png",
+      "assets/images/cristiano4.png",
+      "assets/images/cristiano5.png",
+      "assets/images/cristiano1.png",
+      "assets/images/cristiano2.png",
+      "assets/images/cristiano3.png",
+      "assets/images/cristiano4.png",
+      "assets/images/cristiano5.png",
+    ],
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    final imageList = images[title] ?? [];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple[50],
+        title: Text(
+          title,
+          style: const TextStyle(color: Colors.black),
+        ),
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: imageList.isEmpty
+          ? const Center(
+        child: Text(
+          "No images available",
+          style: TextStyle(fontSize: 18),
+        ),
+      )
+          : GridView.builder(
+        padding: const EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+        ),
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: InteractiveViewer(
+                      child: Image.asset(
+                        imageList[index],
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset(
+                imageList[index],
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 }
+
